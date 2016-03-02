ac4894fe5157056c355389c5575f23ffd95154d5
kpnava <kennethpaulnava@gmail.com>
2016-03-02 09:00:19 +0800

    auto scraping added

---
+++ server/methods/admin/aggregator-task.js
@@ -0,0 +1,9 @@
+Meteor.startup(function () {
+
+    Meteor.setInterval(function () {
+        console.log('Auto Scraping...');
+        Meteor.call('scrape', function () {
+            console.log('Done Scraping...');
+        });
+    }, 300000)
+})
